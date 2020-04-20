c9cb74f3f182fcbc88544097f2459287b33c2502
Sergey Kuzmich <contact@kuzmi.ch>
2020-04-20 23:03:36 +0300

    Add API POST method. Wrap http status errors

--- api.go
+++ api.go
@@ -1,10 +1,12 @@
 package sdk
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/pkg/errors"
 )
@@ -29,6 +31,15 @@ func Harvest(accountId string, accessToken string) *API {
 	return &a
 }
 
+// Applies relevant User-Agent, Accept & Authorization
+func (a *API) _addHeaders(req *http.Request) {
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("User-Agent", "github.com/sergeykuzmich/harvest-sdk v"+CLIENT_VERSION)
+	req.Header.Set("Harvest-Account-Id", a.AccountId)
+	req.Header.Set("Authorization", "Bearer "+a.AccessToken)
+}
+
 func (a *API) Get(path string, args Arguments, target interface{}) error {
 	url := fmt.Sprintf("%s%s", a.ApiUrl, path)
 	urlWithParams := fmt.Sprintf("%s?%s", url, args.ToURLValues().Encode())
@@ -46,7 +57,8 @@ func (a *API) Get(path string, args Arguments, target interface{}) error {
 	if res.StatusCode != 200 {
 		var body []byte
 		body, _ = ioutil.ReadAll(res.Body)
-		return errors.Errorf("HTTP request failure on %s: %s", url, string(body))
+		err := errors.New(strconv.Itoa(res.StatusCode))
+		return errors.Wrapf(err, "HTTP request failure on %s: %s", url, string(body))
 	}
 
 	decoder := json.NewDecoder(res.Body)
@@ -59,10 +71,36 @@ func (a *API) Get(path string, args Arguments, target interface{}) error {
 	return nil
 }
 
-// Applies relevant User-Agent, Accept & Authorization
-func (a *API) _addHeaders(req *http.Request) {
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("User-Agent", "github.com/sergeykuzmich/harvest-sdk v"+CLIENT_VERSION)
-	req.Header.Set("Harvest-Account-Id", a.AccountId)
-	req.Header.Set("Authorization", "Bearer "+a.AccessToken)
+func (a *API) Post(path string, args Arguments, postData interface{}, target interface{}) error {
+	url := fmt.Sprintf("%s%s", a.ApiUrl, path)
+	urlWithParams := fmt.Sprintf("%s?%s", url, args.ToURLValues().Encode())
+
+	buffer := new(bytes.Buffer)
+	if postData != nil {
+		json.NewEncoder(buffer).Encode(postData)
+	}
+
+	req, _ := http.NewRequest("POST", urlWithParams, buffer)
+	a._addHeaders(req)
+
+	res, err := a.Client.Do(req)
+	if err != nil {
+		return errors.Wrapf(err, "HTTP request failure on %s", url)
+	}
+	defer res.Body.Close()
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		var body []byte
+		body, _ = ioutil.ReadAll(res.Body)
+		err := errors.New(strconv.Itoa(res.StatusCode))
+		return errors.Wrapf(err, "HTTP request failure on %s: %s", url, string(body))
+	}
+
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(target)
+	if err != nil {
+		body, _ := ioutil.ReadAll(res.Body)
+		return errors.Wrapf(err, "JSON decode failed on POST to %s: %s", url, string(body))
+	}
+
+	return nil
 }
