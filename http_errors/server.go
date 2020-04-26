3b49fd95eecb5e1b0fe9b684064cea0b29d6ca3b
Eugene Platonov <eugeneplatonoff@gmail.com>
2020-04-26 19:28:57 +0300

    Implement 'server' error

---
+++ http_errors/server.go
@@ -0,0 +1,33 @@
+package http_errors
+
+import "fmt"
+
+type Server struct {
+	status int
+	path   string
+	body   []byte
+}
+
+func (e *Server) Error() string {
+	return fmt.Sprintf("Server Error: %d %s %s", e.status, e.path, e.body)
+}
+
+func (e *Server) Details() []byte {
+	return e.body
+}
+
+func (e *Server) Status() int {
+	return e.status
+}
+
+func (e *Server) Path() string {
+	return e.path
+}
+
+func createServerError(status int, path string, body []byte) *Server {
+	return &Server{
+		status: status,
+		path: path,
+		body: body,
+	}
+}
---
+++ http_errors/server_test.go
@@ -0,0 +1,52 @@
+package http_errors
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateServerError(t *testing.T) {
+	status := 500
+	path := "/server-error"
+	body := []byte("Server error")
+	errorMessage := fmt.Sprintf("Server Error: %d %s %s", status, path, body)
+
+	err := createServerError(status, path, body)
+
+	assert.Equal(t, err.Status(), status)
+	assert.Equal(t, err.Path(), path)
+	assert.Equal(t, err.Details(), body)
+	assert.Equal(t, err.Error(), errorMessage)
+}
+
+func TestCreateFromServerErrordResponse(t *testing.T) {
+	status := 500
+	path := "/server-error"
+	body := "Server error"
+	errorMessage := fmt.Sprintf("Server Error: %d %s %s", status, path, body)
+
+	req, _ := http.NewRequest("GET", path, nil)
+	res := &http.Response{
+		Status:        "500 Internal Server Error",
+		StatusCode:    status,
+		Proto:         "HTTP/1.1",
+		Body:          ioutil.NopCloser(bytes.NewBufferString(body)),
+		ContentLength: int64(len("")),
+		Request:       req,
+	}
+
+	err := CreateFromResponse(res)
+
+	asServerError, ok := err.(*Server)
+	assert.True(t, ok)
+
+	assert.Equal(t, asServerError.Status(), status)
+	assert.Equal(t, asServerError.Path(), path)
+	assert.Equal(t, asServerError.Details(), []byte(body))
+	assert.Equal(t, asServerError.Error(), errorMessage)
+}
