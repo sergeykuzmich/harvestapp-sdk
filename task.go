3a96991ddc04adcc1bf08b43ff7b98122a20ce8e
Sergey Kuzmich <contact@kuzmi.ch>
2020-05-05 19:00:18 +0300

    Add methods to get tasks in paginated and non-paginated ways

--- task.go
+++ task.go
@@ -2,8 +2,16 @@ package hrvst
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
+
+type tasksResponse struct {
+	NextPage int     `json:"next_page"`
+	Tasks    []*Task `json:"tasks"`
+}
+
+type tasksPaginated func() ([]*Task, tasksPaginated, error)
 
 // Task is a struct to represent Harvest Task, performs:
 //  * `struct` -> `JSON` convertion;
@@ -17,6 +25,54 @@ type Task struct {
 	IsActive          bool      `json:"is_active"`
 	CreatedAt         time.Time `json:"created_at"`
 	UpdatedAt         time.Time `json:"updated_at"`
+}
+
+// getAllTasks forcely returns all Harvest Tasks existed on the Account
+func (a *API) getAllTasks(args Arguments) (tasks []*Task, err error) {
+	tasks = make([]*Task, 0)
+
+	next := func() ([]*Task, tasksPaginated, error) {
+		return a.getPaginatedTasks(args)
+	}
+
+	var pageTasks []*Task
+  for ok := (next != nil); ok; ok = (next != nil) {
+      pageTasks, next, err = next()
+      if err != nil {
+          return nil, err
+      }
+
+      tasks = append(tasks, pageTasks...)
+  }
+
+	return tasks, err
+}
+
+// getPaginatedTasks returns Harvest Tasks as it returns Havest (divided by pages)
+func (a *API) getPaginatedTasks(args Arguments) (tasks []*Task, next tasksPaginated, err error) {
+	tasks = make([]*Task, 0)
+	pagedResponse := &tasksResponse{}
+	err = a.Get("/tasks", args, pagedResponse)
+
+	if pagedResponse.NextPage != 0 {
+		next = func() (tasks []*Task, next tasksPaginated, err error) {
+			args["page"] = strconv.Itoa(pagedResponse.NextPage)
+			return a.GetTasks(args)
+		}
+	}
+
+	return pagedResponse.Tasks, next, err
+}
+
+// GetTasks returns list Harvest Tasks.
+// * args["force"] = "true" - is used to get ALL tasks without breaking to pages
+func (a *API) GetTasks(args Arguments) (tasks []*Task, next tasksPaginated, err error) {
+	if args["paged"] == "false" {
+		tasks, err = a.getAllTasks(args)
+		return tasks, nil, err
+	} else {
+		return a.getPaginatedTasks(args)
+	}
 }
 
 // GetTask returns Harvest Task with specified ID.
