6142db68a8ee13652ffb822e7532de912c3516c7
Sergey Kuzmich <accounts@kuzmi.ch>
2024-05-18 02:03:42 +0200

    refactor: Remove unnecessary condition for setting query string values

--- arguments.go
+++ arguments.go
@@ -2,7 +2,6 @@ package hrvst
 
 import (
 	"net/url"
-	"strings"
 )
 
 // Arguments [`key`=>`value`] dictionary should be passed as a GET query string
@@ -17,10 +16,7 @@ func Defaults() Arguments {
 func (args Arguments) toURLValues() url.Values {
 	v := url.Values{}
 	for key, value := range args {
-		// Skip all internal flags while making query string
-		if !strings.HasPrefix(key, flags.Prefix) {
-			v.Set(key, value)
-		}
+		v.Set(key, value)
 	}
 	return v
 }
